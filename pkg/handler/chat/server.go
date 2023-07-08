f3fa456927fbd61c72c4e30f1883fecd027fb81d
kyosu-1 <sho013039@gmail.com>
2023-07-08 16:44:06 +0900

    feat: CreateRoom

--- pkg/handler/chat/server.go
+++ pkg/handler/chat/server.go
@@ -36,9 +36,12 @@ func New(logger log.Handler, chatInteractor chat.Interactor) protoconnect.ChatSe
 }
 
 func (s *server) CreateRoom(ctx context.Context, req *connect.Request[proto.CreateRoomRequest]) (*connect.Response[proto.CreateRoomResponse], error) {
-	// TODO: s.chatInteracter.CreateRoomを実行する
+	room, err := s.chatInteractor.CreateRoom(ctx, req.Msg.GetName())
+	if err != nil {
+		return nil, err
+	}
 	return connect.NewResponse(&proto.CreateRoomResponse{
-		// TODO: Id: room.IDを返却する
+		Id: room.ID,
 	}), nil
 }
 
